a81233271f7a1514b2b0fb06e78b444ae20d9959
Karicharla GNV Swamy Naidu <57288392+GaneshKaricharla2000@users.noreply.github.com>
2020-05-04 13:04:45 +0530

    Added win_pip Module to automate pip process

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+name="winpip"
+author="Karicharla GNV Swamy Naidu"
+short_description="This Lets the windows interpreter programmer to manage pip commands in python programme"
+
+
+
+)
